9bf700617b7248d642e75f3c9501f669dfbd75c0
Muhammed Thasneem P Android <muhammed.thasneem@yahoo.com>
2017-07-18 17:05:19 +0530

    initial commit

---
+++ IOSWebservice/IOSWebservice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSWebservice
+//
+//  Created by Muhammed Thasneem on 18/07/17.
+//  Copyright © 2017 Muhammed Thasneem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
